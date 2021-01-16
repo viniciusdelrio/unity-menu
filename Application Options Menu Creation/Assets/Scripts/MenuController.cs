de6cf3ed14f180c13591fc98b4688e635a035bb9
vinicius-delrio <vinicius.delrio41@gmail.com>
2021-01-16 18:24:45 -0300

    Added DOTween library and make some tween effects

--- Application Options Menu Creation/Assets/Scripts/MenuController.cs
+++ Application Options Menu Creation/Assets/Scripts/MenuController.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,7 +7,27 @@ namespace MyMenu
 {
     public class MenuController : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private TextMeshProUGUI gameTitle;
+        [SerializeField] private RectTransform menuOptions;
         [SerializeField] private GameObject optionsPanel;
+
+        [Header("Tween Parameters")]
+        [SerializeField] private float gameTitleFadeDuration = 2.0f;
+        [SerializeField] private float gameTitleTweenStartDelay = 1.0f;
+        [SerializeField] private float menuOptionsMoveDuration = 1.0f;
+        [SerializeField] private float menuOptionsTweenStartDelay = 2.0f;
+
+        private void Start()
+        {
+            gameTitle.DOFade(1.0f, gameTitleFadeDuration)
+                     .From(0.0f)
+                     .SetDelay(gameTitleTweenStartDelay);
+
+            menuOptions.DOAnchorPos(menuOptions.anchoredPosition, menuOptionsMoveDuration)
+                       .From(new Vector2(657, -25))
+                       .SetDelay(menuOptionsTweenStartDelay);
+        }
 
         public void Play() =>
             SceneManager.LoadSceneAsync("Game Scene");
